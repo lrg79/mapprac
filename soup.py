93c1f71c34095ae7c3415735e9a2b5dc3760ba21
Leah Greenstein <lrg79@cornell.edu>
2016-06-16 11:25:25 -0400

    Update soup.py
    spaghetti code, but getting closer

--- soup.py
+++ soup.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
+import json
 
 soup = BeautifulSoup(open("verizon.html"),from_encoding="utf-8")
 
-print("'" + str(soup.encode("ascii")) + "'")
+#print("'" + str(soup.encode("ascii")) + "'")
 
 arr = []
+response = [] #this stores the json
 
 class prefPrice:
 
@@ -15,7 +17,7 @@ class prefPrice:
 print "*******************"
 
 for tr in soup.findAll('tr'):
-	print(tr)
+	#print(tr)
 	yesNo = "None"
 	countryName = "None"
 	yesNo = tr.find('td', {'class': 'tr0 td22'})
@@ -36,8 +38,37 @@ for tr in soup.findAll('tr'):
 
 	if (str(countryName) != "None") and yesNo != "None":
 		arr.append(prefPrice(countryName,yesNo))
+		for child in countryName:
+    			countryName = child
+		response.append({'country': str(countryName), 'offered': yesNo})
+	else:
+		yesNo = tr.find('td', {'class': 'tr2 td22'})
+		yesNo = str(yesNo)
+	
+		if "Yes" in yesNo:
+			yesNo = "yes"
+		elif "No" in yesNo:
+			yesNo = "no"
+		else:
+			yesNo = "None"
 
-	print yesNo
-	print countryName
+		countryName = tr.find('td', {'class': 'tr2 td18'})
+		str(countryName)
+
+		if str(countryName) != "None":
+			ountryName = countryName.find('p')
+		
+		if (str(countryName) != "None") and yesNo != "None":
+			arr.append(prefPrice(countryName,yesNo))
+			for child in countryName:
+    				countryName = child
+			response.append({'country': str(countryName), 'offered': yesNo})
 
 print len(arr)
+print json.dumps(response)
+x = 0 #view the array of obejcts we built
+while x < len(arr):
+	prefPrice= arr[x]
+	print prefPrice.countryName
+	print prefPrice.yesNo
+	x+=1
