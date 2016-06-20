0fbafe267aa4777e79000c8a48784717587e25cb
Leah Greenstein <lrg79@cornell.edu>
2016-06-20 08:33:43 -0400

    Update soup.py

--- soup.py
+++ soup.py
@@ -28,37 +28,31 @@ class readPDF:
 				yesNo = "None"
 			countryName = tr.find('td', {'class': 'tr' + str(num) + ' ' + 'td18'})
 			str(countryName)
-
+			asGo = ""
 			if str(countryName) != "None":
 				countryName = countryName.find('p')
+				for tr19 in tr.findAll('td', {'class': 'tr' + str(num) + ' ' + 'td19'}):
+					if str(tr19.find('p', 'p20 ft1')) != "None":
+						asGo = (tr19.find('p', 'p20 ft1')) 
 
-			asGo = ""
+					elif str(tr19.find('p', 'p9 ft1')) != "None":
+						asGo = (tr19.find('p', 'p9 ft1'))
 
-			for tr19 in tr.findAll('td', {'class': 'tr' + str(num) + ' ' + 'td19'}):
-				if str(tr19.find('p', 'p20 ft1')) != "None":
-					asGo = (tr19.find('p', 'p20 ft1')) 
+					elif str(tr19.find('p', 'p20 ft4')) != "None":
+						asGo = (tr19.find('p', 'p20 ft4'))
 
-				elif str(tr19.find('p', 'p9 ft1')) != "None":
-					asGo = (tr19.find('p', 'p9 ft1'))
-
-				elif str(tr19.find('p', 'p20 ft4')) != "None":
-					asGo = (tr19.find('p', 'p20 ft4'))
-
-				elif str(tr19.find('p', 'p7 ft4')) != "None":
-					asGo = (tr19.find('p', 'p7 ft4'))
-				else:
-					asGo = "None"
-
-			if(str(asGo) != "None"):
-				for child in asGo:
-						asGo = child
-
-			asGo = str(asGo)
-			print asGo
+					elif str(tr19.find('p', 'p7 ft4')) != "None":
+						asGo = (tr19.find('p', 'p7 ft4'))
+					else:
+						asGo = "None"
 
 			if (str(countryName) != "None") and yesNo != "None":
 				for child in countryName:
-		    			countryName = child
+						countryName = child
+				if asGo != "None":
+					for child in asGo:
+							asGo = child
+							print asGo
 				self.response.append({'country': str(countryName), 'offered': yesNo, 'asGo': asGo})
 
 	def printJSON(self):
